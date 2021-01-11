916f1a973c051b21c257d8055b88530cec3bafc0
xialvjun <xialvjun@didiglobal.com>
2021-01-11 20:20:11 +0800

    feat: setup return render function

--- packages/reactivue/src/defineComponent.ts
+++ packages/reactivue/src/defineComponent.ts
@@ -1,12 +1,12 @@
 import { UnwrapRef } from '@vue/reactivity'
 import { useSetup } from './useSetup'
 
-export function defineComponent<PropsType, State>(
-  setupFunction: (props: PropsType) => State,
-  renderFunction: (state: UnwrapRef<State>) => JSX.Element,
+export function defineComponent<PropsType, State=any>(
+  setupFunction: (props: PropsType) => State | () => JSX.Element,
+  renderFunction?: (state: UnwrapRef<State>) => JSX.Element,
 ): (props: PropsType) => JSX.Element {
   return (props: PropsType) => {
-    const state = useSetup(setupFunction, props)
-    return renderFunction(state)
+    const stateOrRender = useSetup(setupFunction, props)
+    return renderFunction ? renderFunction(stateOrRender) : stateOrRender()
   }
 }
